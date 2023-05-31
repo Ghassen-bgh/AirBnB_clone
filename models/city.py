dd9b431c19e9186659ee7cd6051af2ef146bc1b4
Ghassen-bgh <3591@holbertonstudents.com>
2023-05-31 15:52:22 +0200

    city model

--- models/city.py
+++ models/city.py
@@ -5,9 +5,11 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ City class definition """
-    state_id = ""
-    name = ""
-
     def __init__(self, *args, **kwargs):
-        """ ctor of City class """
-        super().__init__(**kwargs)
+        self.state_id = ""
+        self.name = ""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return "[{}] ({}) {} - {}".format(
+            self.__class__.__name__, self.id, self.name, self.state_id)
