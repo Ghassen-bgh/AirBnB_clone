f6f66f917cf0bcf6a8482add8d9abcda0d35a79d
Ghassen-bgh <3591@holbertonstudents.com>
2023-05-31 15:46:39 +0200

    user model

--- models/user.py
+++ models/user.py
@@ -8,10 +8,14 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """ User class
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-
     def __init__(self, *args, **kwargs):
-        super().__init__(**kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return "[{}] ({}) {} {}".format(
+            self.__class__.__name__, self.id, self.email, self.__dict__)
+
