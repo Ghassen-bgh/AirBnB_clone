59d5be3b607450facabe449fb71c4fcd0aa5420b
Ghassen-bgh <3591@holbertonstudents.com>
2023-05-31 15:54:49 +0200

    review model

--- models/review.py
+++ models/review.py
@@ -5,6 +5,12 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Review class definition """
-    place_id = ""
-    user_id = ""
-    text = ""
+    def __init__(self, *args, **kwargs):
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return "[{}] ({}) {} - {}".format(
+            self.__class__.__name__, self.id, self.text, self.place_id)
